7c11f611b8b49567bfeadf58a65939990a42bc8f
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-04-11 11:15:40 -0300

    :tada: Commit Inicial

---
+++ helloword/src/helloword/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloword;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("A turma Java 82 é TOP!");
+	
+	}
+
+}
