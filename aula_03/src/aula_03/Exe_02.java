4fc8abcf6d0c46c99797a315904a534629ca0d6a
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-04-15 14:01:30 -0300

    :sparkles: Aula 03 - Exercício 02 - Verificação com if

--- aula_03/src/aula_03/Exe_02.java
+++ aula_03/src/aula_03/Exe_02.java
@@ -1,34 +1,10 @@
 package aula_03;
 
-import java.util.Scanner;
-
 public class Exe_02 {
 
-    public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
-        
-        int numero;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-        System.out.print("Digite um número: ");
-        numero = leia.nextInt();
-        
-        if (numero == 0) {
-            System.out.println("\nO número é zero (neutro) e é considerado par.");
-        } else {
-            boolean par = numero % 2 == 0;
-            boolean positivo = numero > 0;
-            
-            if (par && positivo) {
-                System.out.printf("\nO número %d é par e positivo!\n", numero);
-            } else if (!par && positivo) {
-                System.out.printf("\nO número %d é ímpar e positivo!\n", numero);
-            } else if (par && !positivo) {
-                System.out.printf("\nO número %d é par e negativo!\n", numero);
-            } else {
-                System.out.printf("\nO número %d é ímpar e negativo!\n", numero);
-            }
-        }
+	}
 
-        leia.close(); 
-    }
 }
