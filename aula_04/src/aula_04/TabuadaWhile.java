a37ad8be2314577b9b9626d878bdcc5f296be8f1
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-04-16 15:28:47 -0300

    :sparkles: Aula 04 - Laços de Repetição

--- aula_04/src/aula_04/TabuadaWhile.java
+++ aula_04/src/aula_04/TabuadaWhile.java
@@ -2,20 +2,20 @@
 
 import java.util.Scanner;
 
-public class TabuadaFor {
+public class TabuadaWhile {
 
 	public static void main(String[] args) {
 		
-		Scanner leia = new Scanner(System.in);
+        Scanner leia = new Scanner(System.in);
+        int contador = 1;
 		
 		System.out.println("Digite um número inteiro: ");
 		int numero = leia.nextInt();
 		
-		for(int contador = 1; contador <= 10; contador++) {
+		while(contador <= 10) {
 			System.out.printf("\n %d x %d = %d", numero, contador, (numero * contador));
+			contador++;
 		}
-		
-		
 		
 		leia.close();
 
