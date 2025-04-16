02f2a9b6b3773548f01f07b7df8452bae6cc45dc
Rafaela <rafaela_giometti@hotmail.com>
2025-04-16 15:32:01 -0300

    Aula 04 - Laços de Repetição - DoWhile

--- aula_04/TabuadaDoWhile.java
+++ aula_04/TabuadaDoWhile.java
@@ -1,9 +1,23 @@
 package aula_04;
 
+import java.util.Scanner;
+
 public class TabuadaDoWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		  Scanner leia = new Scanner(System.in);
+	        int contador = 11;
+			
+			System.out.println("Digite um número inteiro: ");
+			int numero = leia.nextInt();
+			
+			do {
+				System.out.printf("\n %d x %d = %d", numero, contador, (numero * contador));
+				contador++;
+			}while(contador <= 10);
+			
+			leia.close();
 
 	}
 
