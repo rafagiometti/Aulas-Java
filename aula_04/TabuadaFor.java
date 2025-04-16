d63e71797e7165b790fc615589a791908ace8461
Rafaela <rafaela_giometti@hotmail.com>
2025-04-16 15:33:34 -0300

    ✨ Aula 04 - Laços de Repetição - FOR

--- aula_04/TabuadaFor.java
+++ aula_04/TabuadaFor.java
@@ -1,10 +1,22 @@
 package aula_04;
 
+import java.util.Scanner;
+
 public class TabuadaFor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite um número inteiro: ");
+		int numero = leia.nextInt();
+		
+		for(int contador = 1; contador <= 10; contador++) {
+			System.out.printf("\n %d x %d = %d", numero, contador, (numero * contador));
+		}
+		
+		
+		
+		leia.close();
 
 	}
-
-}
