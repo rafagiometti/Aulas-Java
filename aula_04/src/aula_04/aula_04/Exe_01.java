d930b4c4e4fafb7012dd6b9485789240fc7fcf02
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-04-21 13:15:59 -0300

    :sparkles: Aula 04 - Exercício 01 - Laço de Repetição For

---
+++ aula_04/src/aula_04/aula_04/Exe_01.java
@@ -0,0 +1,30 @@
+package aula_04;
+
+import java.util.Scanner;
+
+public class Exe_01 {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int numero1, numero2;
+        int i;
+
+        System.out.println("Digite o primeiro número do intervalo:");
+        numero1 = leia.nextInt();
+
+        System.out.println("Digite o segundo número do intervalo:");
+        numero2 = leia.nextInt();
+
+        if (numero1 >= numero2) {
+            System.out.println("Intervalo Inválido, o primeiro número deve ser menor que o segundo");
+        } else {
+            for (i = numero1; i <= numero2; i++) {
+                if ((i % 3 == 0) && (i % 5 == 0)) {
+                    System.out.printf("%d é múltiplo de 3 e 5\n", i);
+                }
+            }
+        }
+
+        leia.close();
+    }
+}
