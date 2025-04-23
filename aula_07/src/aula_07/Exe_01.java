4bea0c55927aa1685dbd01d75a2f2509235d8045
Rafaela <rafaela_giometti@hotmail.com>
2025-04-23 15:44:31 -0300

    ✨ Aula 07 - Exercício 01 - Menu Gerenciador de fila com Queue

--- aula_07/src/aula_07/Exe_01.java
+++ aula_07/src/aula_07/Exe_01.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
  
 public class Exe_01 {
-
+ 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		Queue<String> fila = new LinkedList<String>();
